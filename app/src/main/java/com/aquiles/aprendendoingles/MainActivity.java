9c2839f4962bb7ff308f30e7646053f4f2be23d4
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-10-23 22:25:16 -0300

    initial commit

---
+++ app/src/main/java/com/aquiles/aprendendoingles/MainActivity.java
@@ -0,0 +1,43 @@
+package com.aquiles.aprendendoingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.aquiles.aprendendoingles.Fragments.BichosFragment;
+import com.aquiles.aprendendoingles.Fragments.NumerosFragment;
+import com.aquiles.aprendendoingles.Fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        // remover a sobra bottom da action bar
+        getSupportActionBar().setElevation(0);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
